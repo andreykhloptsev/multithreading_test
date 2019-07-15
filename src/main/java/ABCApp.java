b7a3cc6df0c4c05e4ac31d178b7655bbceb81d4c
andreykhloptsev <ahloptsev@gmail.com>
2019-07-15 19:23:03 +0300

    MFU app

--- src/main/java/ABCApp.java
+++ src/main/java/ABCApp.java
@@ -1,9 +1,15 @@
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+
 /**
- * Created by DIO
+ * Создать три потока, каждый из которых выводит определенную букву (A, B и C) 5 раз.
+ * Порядок должен быть именно таким: ABСABСABС. Используйте wait/notify/notifyAll.
  */
 public class ABCApp {
     public static void main(String[] args) {
         ABC abc = new ABC();
+
+
         Thread th1 = new Thread() {
             @Override
             public void run() {
